d0bb676be65579a562dc483fb236b4ee234c5712
ramaalshaban <50273459+ramaalshaban@users.noreply.github.com>
2022-04-19 18:20:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -26,25 +26,3 @@ function destructivelyDeleteFromEmployeeByKey(employee, key){
     return employee
 }
 
-// const employee = {name: "Sam",
-// streetAddress: "11 Broadway",};
-
-// function updateEmployeeWithKeyAndValue(employee,key,value) {
-//     const newObj ={...employee};
-//     newObj.name = "Sam";
-//     newObj.streetAddress = "11 Broadway";
-//     return newObj;
-// }
-// function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value) {
-//     employee.streetAddress= "12 Broadway";
-//     return employee;
-// }
-// function deleteFromEmployeeByKey(employee,key,value) {
-//     const newEmployee = {...employee};
-//     delete newEmployee.name;
-//     return newEmployee;
-// }
-// function destructivelyDeleteFromEmployeeByKey(employee,key,value) {
-//     delete employee.name;
-//     return employee;
-// }
